03deb2ce1c21a0ff6fa2837688ece29ae1d44607
AskC <andycheung_@hotmail.com>
2022-12-31 16:21:20 +0100

    Tutorial until 2.24: Building messages system, included some style from Angular Material

---
+++ mean-course/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  // Test data
+  // posts = [
+  //   {title: 'First Post', content: 'First post content'},
+  //   {title: 'Second Post', content: 'Second post content'},
+  //   {title: 'Third Post', content: 'Third post content'},
+  // ]
+
+  @Input() posts = [];
+
+}
