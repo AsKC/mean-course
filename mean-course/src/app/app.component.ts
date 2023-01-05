c304fccf1771eb4cd5834486698f775de901d5b9
AskC <andycheung_@hotmail.com>
2023-01-05 14:03:15 +0100

    Added custom test to check API call to our own back-end REST API.

--- mean-course/src/app/app.component.ts
+++ mean-course/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getMembers();
+    this.getCountry();
+  }
+
+  getMembers() {
+    this.http.get('http://localhost:8080/api/employees').subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+
+  getCountry() {
+    this.http.get('http://localhost:8080/info/country').subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
 }
